80f55e00ca4167fabbf5519a849f6cb979cbafd7
Joaquin Serra caggiano <joaquinserracaggiano@MacBook-Pro-de-Joaquin-2.local>
2025-10-16 20:32:10 +0200

    feat(): swagger in dtos

--- src/common/dtos/pagination.dto.ts
+++ src/common/dtos/pagination.dto.ts
@@ -1,7 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import { IsNumber, IsOptional, IsPositive, Min } from 'class-validator';
 
 export class PaginationDto {
+  @ApiProperty({
+    example: 10,
+    description: 'Limit of items per page',
+    default: 10,
+  })
   @IsOptional()
   @IsNumber()
   @IsPositive()
@@ -9,6 +15,11 @@ export class PaginationDto {
   @Type(() => Number) // Transform the value to a number
   limit?: number;
 
+  @ApiProperty({
+    example: 0,
+    description: 'Offset of items per page',
+    default: 0,
+  })
   @IsOptional()
   @IsNumber()
   @Min(0)
