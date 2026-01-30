f30bf7595b4c9399208934be7ad9de10db169d97
Joaquín Caggiano <joaquincaggiano@MacBook-Pro-de-Joaquin.local>
2026-01-30 14:06:51 +0100

    feat(): register and remove connection

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -1,17 +1,22 @@
-import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway } from '@nestjs/websockets';
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  WebSocketGateway,
+} from '@nestjs/websockets';
 import { Socket } from 'socket.io';
 import { MessagesWsService } from './messages-ws.service';
 
 @WebSocketGateway({ cors: true })
-export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconnect {
+export class MessagesWsGateway
+  implements OnGatewayConnection, OnGatewayDisconnect
+{
+  constructor(private readonly messagesWsService: MessagesWsService) {}
 
-  constructor(private readonly messagesWsService: MessagesWsService) { }
-
-  handleConnection(client: Socket,) {
-    console.log('Cliente conectado: ', client.id);
+  handleConnection(client: Socket) {
+    this.messagesWsService.registerClient(client);
   }
 
   handleDisconnect(client: Socket) {
-    console.log('Cliente desconectado: ', client.id);
+    this.messagesWsService.removeClient(client.id);
   }
 }
